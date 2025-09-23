54dfa5fc7b195b6acb5c1b8cc08da41c3cc2f9ef
idrojone <jordivallspladaw@gmail.com>
2025-09-23 12:19:42 +0200

    añadimos la carpeta backend

--- Backend/UserServices/app/routes/concierto.routes.js
+++ Backend/UserServices/app/routes/concierto.routes.js
@@ -2,5 +2,5 @@ module.exports = (app) => {
     const conciertos = require('../controllers/concierto.controller.js');
 
     //Get all conciertos
-    app.get('/conciertos', conciertos.findAllConciertos);
+    app.get('/api/conciertos', conciertos.findAllConciertos);
 };
