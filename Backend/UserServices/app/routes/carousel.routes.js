82bf2a819b494506c0dc22dbd4e37f84815d46c7
Jordi Valls Pla <jordivallspladaw@gmail.com>
2025-09-26 17:07:41 +0200

    feat: carousel conciertos y mejoras modelo
    - Controller carousel para gestión de carousels
    - Actualizaciones en modelo concierto
    - Mejoras en SpotifyAPI utility

--- Backend/UserServices/app/routes/carousel.routes.js
+++ Backend/UserServices/app/routes/carousel.routes.js
@@ -3,6 +3,10 @@ module.exports = (app) => {
     const carousel = require('../controllers/carousel.controller.js');
 
     // Generos para el carousel
-    app.get('/api/carousel', carousel.findAllGenerosCarousel);
+    app.get('/api/carousel/generos', carousel.findAllGenerosCarousel);
+
+    // Conciertos para el carousel
+    app.get('/api/carousel/conciertos', carousel.getAllConciertosCarrusel);
+    app.get('/api/carousel/conciertos/:slug', carousel.getOneConciertoCarrusel);
 
 };
