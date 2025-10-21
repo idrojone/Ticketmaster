e9446871f256b2c2616b57c8e54a3f308d531792
Jordi Valls Pla <jordivallspladaw@gmail.com>
2025-10-21 19:34:45 +0200

    extraer helpers de favoritos y following

--- Backend/UserServices/app/models/user.model.js
+++ Backend/UserServices/app/models/user.model.js
@@ -78,55 +78,9 @@ userSchema.methods.generateAccessToken = function() {
 
 userSchema.methods.toUserResponse = async function() {
     console.log('User Model - toUserResponse called');
-  
-    const favIds = this.favouriteConciertos
-        .map(c => {
-            if (!c) return null;
-            if (c._id) return c._id.toString();
-            return c.toString();
-        })
-        .filter(id => mongoose.Types.ObjectId.isValid(id));
+    const favSlugs = await this.getFavouriteSlugs();
 
-
-    // Obtener slugs en una sola consulta
-    let favSlugs = [];
-    if (favIds.length) {
-        const Concierto = mongoose.model('Concierto');
-        const conciertos = await Concierto.find({ _id: { $in: favIds } })
-            .select('slug')
-            .lean();
-
-        const slugMap = new Map(conciertos.map(c => [c._id.toString(), c.slug]));
-
-        // Mantener el orden original de favouriteConciertos, devolviendo null si no existe
-        favSlugs = this.favouriteConciertos.map(c => {
-            const id = c && c._id ? c._id.toString() : (c ? c.toString() : null);
-            return id && slugMap.has(id) ? slugMap.get(id) : null;
-        });
-    }
-
-    const followingIds = this.followingUsers
-        .map(u => {
-            if (!u) return null;
-            if (u._id) return u._id.toString();
-            return u.toString();
-        })
-        .filter(id => mongoose.Types.ObjectId.isValid(id));
-
-    let followingUsernames = [];
-    if (followingIds.length) {
-        const User = mongoose.model('User');
-        const users = await User.find({ _id: { $in: followingIds } })
-            .select('username')
-            .lean();
-
-        const userMap = new Map(users.map(u => [u._id.toString(), u.username]));
-
-        followingUsernames = this.followingUsers.map(u => {
-            const id = u && u._id ? u._id.toString() : u ? u.toString() : null;
-            return id && userMap.has(id) ? userMap.get(id) : null;
-        });
-    }
+    const followingUsernames = await this.getFollowingUsernames();
 
 
     return {
@@ -136,70 +90,15 @@ userSchema.methods.toUserResponse = async function() {
         email: this.email,
         bio: this.bio,
         image: this.image,
-        favouriteConciertos: favSlugs.filter(Boolean),
-        followingUsers: followingUsernames.filter(Boolean),
+        favouriteConciertos: favSlugs,
+        followingUsers: followingUsernames,
         token: this.generateAccessToken(),
     };
 };
 
 userSchema.methods.toUserDetails = async function() {
-
-    // sacar slug del Concierto a raiz del _id
-    const favIds = this.favouriteConciertos
-        .map((c) => {
-            if (!c) return null;
-            if (c._id) return c._id.toString();
-            return c.toString();
-        })
-        .filter((id) => mongoose.Types.ObjectId.isValid(id));
-
-    const favConciertoIds = this.favouriteConciertos.map((c) => {
-        if (!c) return null;
-        return c.toString
-    }).filter((id) => mongoose.Types.ObjectId.isValid(id));
-
-    console.log(favConciertoIds)
-
-    // Obtener slugs en una sola consulta
-    let favSlugs = [];
-    if (favIds.length) {
-        const Concierto = mongoose.model("Concierto");
-        const conciertos = await Concierto.find({ _id: { $in: favIds } }).select("slug").lean();
-        const slugMap = new Map(
-            conciertos.map((c) => [c._id.toString(), c.slug])
-        );
-
-        favSlugs = this.favouriteConciertos.map((c) => {
-            const id = c && c._id ? c._id.toString() : c ? c.toString() : null;
-            return id && slugMap.has(id) ? slugMap.get(id) : null;
-        });
-    }
-
-    // sacar el username del User que le sigue a raíz del _id
-    const followingIds = this.followingUsers
-        .map((u) => {
-            if (!u) return null;
-            if (u._id) return u._id.toString();
-            return u.toString();
-        })
-        .filter((id) => mongoose.Types.ObjectId.isValid(id));
-
-    let followingUsernames = [];
-    if (followingIds.length) {
-        const User = mongoose.model("User");
-        const users = await User.find({ _id: { $in: followingIds } })
-            .select("username")
-            .lean();
-
-        const userMap = new Map(
-            users.map((u) => [u._id.toString(), u.username])
-        );
-
-        followingUsernames = this.followingUsers.map((u) => {
-            const id = u && u._id ? u._id.toString() : u ? u.toString() : null;
-            return id && userMap.has(id) ? userMap.get(id) : null;
-        });
-    }
+    const favSlugs = await this.getFavouriteSlugs();
+    const followingUsernames = await this.getFollowingUsernames();
 
     return {
         _id: this._id,
@@ -208,8 +107,8 @@ userSchema.methods.toUserDetails = async function() {
         email: this.email,
         bio: this.bio,
         image: this.image,
-        favouriteConciertos: favSlugs.filter(Boolean),
-        followingUsers: followingUsernames.filter(Boolean),
+        favouriteConciertos: favSlugs,
+        followingUsers: followingUsernames,
     };
 };
 
@@ -251,6 +150,54 @@ userSchema.methods.unlikeConcierto = async function(conciertoId) {
         (id) => !id.equals(conciertoId)
     );
     await this.save();
+};
+
+userSchema.methods.getFollowingUsernames = async function() {
+    const followingIds = this.followingUsers
+        .map((u) => {
+            if (!u) return null;
+            if (u._id) return u._id.toString();
+            return u.toString();
+        })
+        .filter((id) => mongoose.Types.ObjectId.isValid(id));
+
+    if (!followingIds.length) return [];
+
+    const User = mongoose.model('User');
+    const users = await User.find({ _id: { $in: followingIds } })
+        .select('username')
+        .lean();
+
+    const userMap = new Map(users.map((u) => [u._id.toString(), u.username]));
+
+    return this.followingUsers.map((u) => {
+        const id = u && u._id ? u._id.toString() : u ? u.toString() : null;
+        return id && userMap.has(id) ? userMap.get(id) : null;
+    }).filter(Boolean);
+};
+
+userSchema.methods.getFavouriteSlugs = async function() {
+    const favIds = this.favouriteConciertos
+        .map((c) => {
+            if (!c) return null;
+            if (c._id) return c._id.toString();
+            return c.toString();
+        })
+        .filter((id) => mongoose.Types.ObjectId.isValid(id));
+
+    if (!favIds.length) return [];
+
+    const Concierto = mongoose.model('Concierto');
+    const conciertos = await Concierto.find({ _id: { $in: favIds } })
+        .select('slug')
+        .lean();
+
+    const slugMap = new Map(conciertos.map((c) => [c._id.toString(), c.slug]));
+
+    return this.favouriteConciertos.map((c) => {
+        const id = c && c._id ? c._id.toString() : c ? c.toString() : null;
+        return id && slugMap.has(id) ? slugMap.get(id) : null;
+    }).filter(Boolean);
 };
 
 module.exports = mongoose.model('User', userSchema);
